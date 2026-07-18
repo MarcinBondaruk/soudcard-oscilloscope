b1ad06e94dadfa2c9e65bd2eeea8a02b28aa766a
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2026-07-18 15:29:07 +0200

    portaudio initial

--- main.go
+++ main.go
@@ -2,58 +2,13 @@ package main
 
 import (
 	"fmt"
-	"image/color"
-	"math"
 
 	"github.com/gordonklaus/portaudio"
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/vector"
 )
-
-const (
-	screenWidth  = 800
-	screenHeight = 400
-)
-
-type Game struct {
-	waveform []float32
-}
-
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.RGBA{15, 20, 30, 255})
-	centerY := float32(screenHeight / 2)
-	maxWaveHeight := float32(screenHeight/2) * 0.9
-
-	waveColor := color.RGBA{0, 210, 255, 255}
-	xAxisColor := color.RGBA{40, 50, 70, 255}
-
-	vector.StrokeLine(screen, 0, centerY, float32(screenWidth), centerY, 1, xAxisColor, false)
-
-	for x := 0; x < len(g.waveform); x++ {
-		amplitude := g.waveform[x]
-
-		offsetY := amplitude * maxWaveHeight
-
-		xPos := float32(x)
-		y1 := centerY - offsetY
-		y2 := centerY + offsetY
-		vector.StrokeLine(screen, xPos, y1, xPos, y2, 1, waveColor, false)
-	}
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return screenWidth, screenHeight
-}
 
 func main() {
 	samplingRate := 48 * 1000
 	duration := 4
-	// fps := 60
-	// samplesPerFrame := samplingRate / fps
 	bufferSize := samplingRate * duration
 
 	inputBuffer := make([]float32, bufferSize)
@@ -80,35 +35,9 @@ func main() {
 		return
 	}
 
-	stream.Stop()
+	err = stream.Stop()
 	if err != nil {
 		fmt.Println("error: ", err)
-		return
-	}
-
-	waveform := make([]float32, screenWidth)
-	samplesPerPixel := bufferSize / screenWidth
-
-	for x := 0; x < screenWidth; x++ {
-		startIdx := x * samplesPerPixel
-		endIdx := startIdx + samplesPerPixel
-
-		var maxAmp float32 = 0.0
-		for _, sample := range inputBuffer[startIdx:endIdx] {
-			absSample := float32(math.Abs(float64(sample)))
-			if absSample > maxAmp {
-				maxAmp = absSample
-			}
-		}
-		waveform[x] = maxAmp
-	}
-	game := &Game{
-		waveform: waveform,
-	}
-
-	ebiten.SetWindowTitle("Statyczny Wykres Fali Audio (Waveform)")
-	ebiten.SetWindowSize(screenWidth, screenHeight)
-	if err := ebiten.RunGame(game); err != nil {
 		return
 	}
 }
